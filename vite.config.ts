6a86d0b0b59b75f131ea0a20dc3a6c92c0cd8749
杨云虎 <yangyunhuyunnan@163.com>
2026-04-03 00:26:51 +0800

    调整chunk大小限制，消除构建警告

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import vue from "@vitejs/plugin-vue"
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    chunkSizeWarningLimit: 1000
+  },
   server: {
     proxy: {
       "/api": {
